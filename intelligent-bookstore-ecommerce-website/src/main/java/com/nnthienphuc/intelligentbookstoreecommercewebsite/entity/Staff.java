4b13b99a436a58e41641730fd4428503ab94c76d
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2024-11-08 20:17:25 +0700

    Create 2 entities: Staff and User

--- intelligent-bookstore-ecommerce-website/src/main/java/com/nnthienphuc/intelligentbookstoreecommercewebsite/entity/Staff.java
+++ intelligent-bookstore-ecommerce-website/src/main/java/com/nnthienphuc/intelligentbookstoreecommercewebsite/entity/Staff.java
@@ -1,4 +1,32 @@
 package com.nnthienphuc.intelligentbookstoreecommercewebsite.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Staff {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String staffID;
+    String fullName;
+    String email;
+    String phone;
+    String idCard;
+    boolean roleID;
+    boolean gender;
+    Date birthday;
+    String address;
+    String pwd;
+    boolean isQuit;
 }
--- intelligent-bookstore-ecommerce-website/src/main/java/com/nnthienphuc/intelligentbookstoreecommercewebsite/entity/User.java
+++ intelligent-bookstore-ecommerce-website/src/main/java/com/nnthienphuc/intelligentbookstoreecommercewebsite/entity/User.java
@@ -1,4 +1,27 @@
 package com.nnthienphuc.intelligentbookstoreecommercewebsite.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.Date;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    String userID;
+    String email;
+    String fullName;
+    boolean gender;
+    Date birthDay;
+    String pwd;
 }
