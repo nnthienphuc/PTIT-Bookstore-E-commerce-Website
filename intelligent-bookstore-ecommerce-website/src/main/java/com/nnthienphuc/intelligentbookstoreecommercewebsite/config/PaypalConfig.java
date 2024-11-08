e321606794e5787b5ae619a3c38c98e143645c2b
Nguyen Ngoc Thien Phuc <nnthienphuc@gmail.com>
2024-11-08 19:02:02 +0700

    Edit paypal config

--- intelligent-bookstore-ecommerce-website/src/main/java/com/nnthienphuc/intelligentbookstoreecommercewebsite/config/PaypalConfig.java
+++ intelligent-bookstore-ecommerce-website/src/main/java/com/nnthienphuc/intelligentbookstoreecommercewebsite/config/PaypalConfig.java
@@ -1,5 +1,8 @@
 package com.nnthienphuc.intelligentbookstoreecommercewebsite.config;
 
+import com.paypal.base.rest.APIContext;
+import com.paypal.base.rest.OAuthTokenCredential;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,31 +13,31 @@ import java.util.Map;
 @Configuration
 public class PaypalConfig {
 
-//    @Value("${paypal.client.id}")
-//    private String clientId;
-//
-//    @Value("${paypal.client.secret}")
-//    private String clientSecret;
-//
-//    @Value("${paypal.mode}")
-//    private String mode;
-//
-//    @Bean
-//    public Map<String, String> paypalSdkConfig() {
-//        Map<String, String> configMap = new HashMap<String, String>();
-//        configMap.put("mode", mode);
-//        return configMap;
-//    }
+    @Value("${paypal.client.id}")
+    private String clientId;
 
-//    @Bean
-//    public OAuthTokenCredential oAuthTokenCredential() {
-//        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
-//    }
-//
-//    @Bean
-//    public APIContext context() throws PayPalRESTException {
-//        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
-//        context.setConfigurationMap(paypalSdkConfig());
-//        return context;
-//    }
+    @Value("${paypal.client.secret}")
+    private String clientSecret;
+
+    @Value("${paypal.mode}")
+    private String mode;
+
+    @Bean
+    public Map<String, String> paypalSdkConfig() {
+        Map<String, String> configMap = new HashMap<String, String>();
+        configMap.put("mode", mode);
+        return configMap;
+    }
+
+    @Bean
+    public OAuthTokenCredential oAuthTokenCredential() {
+        return new OAuthTokenCredential(clientId, clientSecret, paypalSdkConfig());
+    }
+
+    @Bean
+    public APIContext context() throws PayPalRESTException {
+        APIContext context = new APIContext(oAuthTokenCredential().getAccessToken());
+        context.setConfigurationMap(paypalSdkConfig());
+        return context;
+    }
 }
